b4110fb6a684e098dc428b46eb6839b515beaa7b
iorguti <adriansilva1900@gmail.com>
2021-04-18 06:26:25 -0300

    First commit

---
+++ zeus-master/src/zeusmenu.js
@@ -0,0 +1,40 @@
+const 𝑫𝑬𝑰𝑫𝑨𝑹𝑨menu = (prefix) => {
+	return `
+
+            COMANDOS:
+
+
+  *Comandos do 𝑫𝑬𝑰𝑫𝑨𝑹𝑨 :*
+
+➸ *${prefix}loli*
+➸ *${prefix}hentai*
+➸ *${prefix}dono*
+➸ *${prefix}porno*
+➸ *${prefix}boanoite*
+➸ *${prefix}bomdia*
+➸ *${prefix}boatarde*
+➸ *${prefix}mia*
+➸ *${prefix}mia1*
+➸ *${prefix}mia2*
+➸ *${prefix}belle*
+➸ *${prefix}belle1*
+➸ *${prefix}belle2*
+➸ *${prefix}belle3*
+➸ *${prefix}ayeko*
+
+╔════════════════════
+  TRADUZIDO POR *𝑫𝑬𝑰𝑫𝑨𝑹𝑨*
+  DUVIDAS? 👇
+  WA.me/555195067276
+╚════════════════════`
+}
+
+exports.𝑫𝑬𝑰𝑫𝑨𝑹𝑨menu = 𝑫𝑬𝑰𝑫𝑨𝑹𝑨menu
+
+
+
+
+
+
+
+
